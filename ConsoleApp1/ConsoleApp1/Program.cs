aeebb7165419406f952f25c7a30befe1e9f6412d
KIMHYEOK\borad <kimmang940715@naver.com>
2024-12-05 14:24:38 +0900

    패스트 캠퍼스 Unity & C# 강의

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace ConsoleApp1
+﻿namespace HelloWorld
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
         }
     }
+
+    // using : 선언된 네임스페이스 내부 개체들에 접근
+    // namespace : 그룹화, 동일한 함수의 이름 사용 가능
+    // class : 필드(field), 메소드로(method)로 분리, 프로그램을 구성하는 기본
+    // 세미콜론 ';' : 코드 문장의 끝
+    // 컬리 브레이스 '{}' : 코드블록(블록 단위로 코드 구분)
+    // 주석 : // 한줄주석 , /* */ 여러줄 주석
+
+    /*
+     * static void Main 함수
+     * 가장 중요한 메소드
+     * 최초의 진입점 (Entry Point)
+     * 메인 메소드가 반드시 존재 
+     */
 }
---
+++ ConsoleApp1/HelloWorld01/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorld01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Hello World : " + args[0]);
+            }
+            else
+            {
+                Console.WriteLine("Hello World args.Lengh = 0 ");
+            }
+        }
+    }
+}
